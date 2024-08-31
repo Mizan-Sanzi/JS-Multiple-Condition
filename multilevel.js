c8ea9914d26b7a73d01a6f0ea9c55dd680eec112
mizansanzi <pleasemailmizan@gmail.com>
2024-08-31 21:31:09 +0600

    code added

--- multilevel.js
+++ multilevel.js
@@ -1,5 +1,5 @@
 // multi level condition
-const price = 5000;
+const price = 2000;
 
 if (price>= 5000){
     // 10% discount
@@ -7,6 +7,28 @@ if (price>= 5000){
     const payAmount = price - discount;
     console.log (payAmount);
 }
+else if (price>2500) {
+// 5% discount
+const discount = price * 5/100;
+const payAmount = price - discount;
+console.log (payAmount);
+}
 else {
     console.log(price);
+}
+
+const age = 62;
+const foodPrice = 500;
+
+if (age<12) {
+    console.log ("you can eat for free");
+}
+else if (age >=60) {
+    // 50% discount
+    const discountPrice = foodPrice*50/100;
+    const paymentAmount = foodPrice-discountPrice;
+    console.log(paymentAmount);
+}
+else {
+    console.log ("Please pay full amount");
 }
