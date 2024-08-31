01aa1684e3028c1577726a3191049f0284f6cb5b
mizansanzi <pleasemailmizan@gmail.com>
2024-08-31 20:41:56 +0600

    code added

--- multiple.js
+++ multiple.js
@@ -1,9 +1,35 @@
 const salary = 25000;
 const isBCS = true;
+const height = 61;
+const hasCar = true;
 
-if (salary>20000) {
+// condition && (and)
+
+if (salary>20000 && height>66) {
     console.log("It is ok.");
 }
 else {
+    console.log("Search for another");
+}
+
+// condition || (or)
+
+console.log("condition 'or' ")
+
+if (salary>25000 || height>72 || isBCS == false) {
+    console.log("It is ok.");
+}
+else {
+    console.log("Search for another");
+}
+
+// complex condition
+
+console.log("complex condition");
+
+if ((salary>30000 && hasCar == true) && isBCS == true) {
+    console.log ("We are agreed")
+}
+else{
     console.log("Search for another");
 }
